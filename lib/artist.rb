9a2346338907ca3fa0588298227a82581bf2215d
Abagail Hudson <abagailanne@gmail.com>
2019-06-05 00:07:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,22 +21,20 @@ class Artist
     self.new(artist).tap {|name| name.save}
   end
 
-   def add_song(song)
+  def add_song(song)
     song.artist = self unless song.artist
     @songs << song unless @songs.include?(song)
   end
 
-   def self.all
+  def self.all
     @@all
   end
 
-   def self.destroy_all
+  def self.destroy_all
     @@all.clear
   end
 
-   def genres #returns a collection of genres for all of the artist's songs (artist has many genres through songs)
-    #does not return duplicate genres if the artist has more than one song of a particular genre (artist has many genres through songs)
-    #collects genres through its songs instead of maintaining its own @genres instance variable (artist has many genres through songs)
+  def genres 
     self.songs.collect do |song|
       song.genre
     end.uniq
